01f4e5d2f468d5187f4ff21468e271e2b83c347e
Brandon <brandon@Brandons-MacBook-Pro-70.local>
2024-12-19 00:29:23 -0500

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,30 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import '../styles/Navbar.css';
 
+
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
   const history = useHistory();
-  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const handleLogout = async () => {
     await logout();
     history.push('/'); // Redirect to home page after logout
   };
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
-
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+      <Link to="/" className="navbar-logo">
           <img src="/playrportal.png" alt="PlayrPortal Logo" className="navbar-logo-img" />
         </Link>
         <ul className="navbar-menu">
-          <li className="navbar-item">
-            <Link to="/" className="navbar-link">Home</Link>
-          </li>
+          {!user && (
+            <li className="navbar-item">
+              <Link to="/" className="navbar-link">Home</Link>
+            </li>
+          )}
 
           {!user && (
             <>
@@ -47,15 +45,8 @@ const Navbar = () => {
               <li className="navbar-item">
                 <Link to="/opportunities" className="navbar-link">Browse Opportunities</Link>
               </li>
-              <li className="navbar-item dropdown">
-                <button onClick={toggleDropdown} className="navbar-button">Menu</button>
-                {dropdownOpen && (
-                  <ul className="dropdown-menu">
-                    <li className="dropdown-item">
-                      <button onClick={handleLogout} className="navbar-button">Logout</button>
-                    </li>
-                  </ul>
-                )}
+              <li className="navbar-item">
+                <button onClick={handleLogout} className="navbar-button">Logout</button>
               </li>
             </>
           )}
@@ -71,15 +62,8 @@ const Navbar = () => {
               <li className="navbar-item">
                 <Link to="/saved-players" className="navbar-link">Saved Players</Link>
               </li>
-              <li className="navbar-item dropdown">
-                <button onClick={toggleDropdown} className="navbar-button">Menu</button>
-                {dropdownOpen && (
-                  <ul className="dropdown-menu">
-                    <li className="dropdown-item">
-                      <button onClick={handleLogout} className="navbar-button">Logout</button>
-                    </li>
-                  </ul>
-                )}
+              <li className="navbar-item">
+                <button onClick={handleLogout} className="navbar-button">Logout</button>
               </li>
             </>
           )}

8a1af6739dffab740e8b09b403e368752b85a4ca
bwu29 <brandon_wu1@alumni.brown.edu>
2024-12-23 17:52:21 -0500

    emails

--- src/App.js
+++ src/App.js
@@ -10,21 +10,20 @@ import SavedPlayers from "./pages/SavedPlayers";
 import Opportunities from "./pages/Opportunities";
 import { init } from '@amplitude/analytics-browser';
 
-// Initialize Amplitude with your API key
-init('5536de7451587432e3c4a5b69028c1ba');
+// Initialize Amplitude with your API key and defaultTracking option
+init('5536de7451587432e3c4a5b69028c1ba', {
+  defaultTracking: true,
+});
 
 const App = () => {
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (window.amplitude) {
-        console.log('Logging App Loaded event...');
-        window.amplitude.getInstance().logEvent('App Loaded');
-        console.log('Amplitude event logged successfully');
-        clearInterval(interval);
-      } else {
-        console.error('Amplitude is not defined');
-      }
-    }, 1000);
+    if (window.amplitude) {
+      console.log('Logging App Loaded event...');
+      window.amplitude.getInstance().logEvent('App Loaded');
+      console.log('Amplitude event logged successfully');
+    } else {
+      console.error('Amplitude is not defined');
+    }
   }, []);
 
   return (
